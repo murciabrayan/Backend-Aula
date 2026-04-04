bba19b4ca53ae5e52cb33ea78f315f222d1f2d86
Brayan <branfer60@gmail.com>
2026-04-04 03:01:20 -0500

    Limita el timeout de correo en produccion

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -212,6 +212,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = int(os.getenv('EMAIL_TIMEOUT', '10'))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'branfer60@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'bnla xiox mgzh wjue')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
